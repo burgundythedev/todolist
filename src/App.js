3dcfa8096b8cc6e75028f759e2b14d3b38e94f4e
Olivier Bourgogne <bourgogne.olivier@gmail.com>
2023-02-23 14:01:16 +0100

    add project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import TodoList from "./components/TodoList";
+import todo from "./UI/Image/todo.png";
+import { Helmet } from "react-helmet";
+import Footer from "./components/Footer";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>ToDO</title>
+        <link rel="canonical" href="http://mysite.com/example" />
+        <meta name="description" content="TodoList" />
+      </Helmet>
+      <div className="header">
+        <img className="img" src={todo} alt="logo" />
+        <h1 className="title">Finally organize your work and your life.</h1>
+      </div>
+      <div className="todo-app">
+        <TodoList />
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div className="footer">
+      <p className="footer-title">developed by O.Bourgogne</p>
+    </div>
+  );
+}
+
+export default Footer;
